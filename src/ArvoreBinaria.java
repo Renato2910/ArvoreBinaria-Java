984aa0817cfd6b23ab78cd44a3856eb159be3a69
Renato Parra Silva <renato.parrasilva11@gmail.com>
2024-07-26 18:16:58 -0300

    Primeiro Commit

---
+++ src/ArvoreBinaria.java
@@ -0,0 +1,65 @@
+public class ArvoreBinaria {
+    private No raiz;
+
+    public ArvoreBinaria() {
+        this.raiz = null;
+    }
+
+    public void inserir(int valor) {
+        No novoNo = new No(valor);
+        if(this.raiz == null) {
+            this.raiz = novoNo;
+        } else {
+            No atual = this.raiz;
+            No pai = null;
+            boolean esquerda = false;
+            while(atual != null) {
+                if(novoNo.getValor() < atual.getValor()) {
+                    pai = atual;
+                    atual = atual.getEsq();
+                    esquerda = true;
+                } else {
+                    pai = atual;
+                    atual = atual.getDir();
+                    esquerda = false;
+                }
+            }
+            if(esquerda) {
+                pai.setEsq(novoNo);
+            } else {
+                pai.setDir(novoNo);
+            }
+        }
+    }
+
+    public No getRaiz() {
+        return this.raiz;
+    }
+
+    public void preOrdem(No no) {
+        if(no == null) {
+            return;
+        }
+        System.out.println(no.getValor());
+        preOrdem(no.getEsq());
+        preOrdem(no.getDir());
+    }
+
+    public void emOrdem(No no) {
+        if(no == null) {
+            return;
+        }
+        emOrdem(no.getEsq());
+        System.out.println(no.getValor());
+        emOrdem(no.getDir());
+    }
+
+    public void posOrdem(No no) {
+        if(no == null) {
+            return;
+        }
+        posOrdem(no.getEsq());
+        posOrdem(no.getDir());
+        System.out.println(no.getValor());
+    }
+}
